c48bc2623151641c6020e9d86f8aaad7c71a0256
Joel-Crunk <joel.crunk@outlook.com>
2019-02-21 19:33:38 -0500

    This commit will take the radius and give the volume of a hemisphere.

--- VolumeOfHemisphere/Program.cs
+++ VolumeOfHemisphere/Program.cs
@@ -10,6 +10,22 @@ namespace VolumeOfHemisphere
     {
         static void Main(string[] args)
         {
+           
+            Console.Write("Enter the radius of a hemisphere: " );
+            //this is the code that gets the information that is needed for r
+            double r = Double.Parse(Console.ReadLine());
+            //r is the radius
+            double r3 = r * r * r;
+            //r3 is the radius squared
+            double Volume = (2 / 3.0) * (Math.PI) * r3;
+            // in (2/3.0) the 3.0 was to make the fraction a double otherwise it 2/3 = 0 and 0 * Math.Pi = 0 and then 0 * r3 = 0 so 
+            // it always returned 0 unless you put the 2/3 after Math.PI because Math.PI is a double thus making 2/3 a double when you 
+            //multiplied the two.
+
+            Console.WriteLine($"The volume of this hemisphere is: {Volume}");
+
+            Console.ReadLine();
+
         }
     }
 }
